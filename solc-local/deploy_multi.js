ab91e954e5af6955e910327426618fe8c270737a
Bruce Lee <coolcode@live.com>
2019-05-14 15:38:04 +0800

    :monkey_face:

--- solc-local/deploy_multi.js
+++ solc-local/deploy_multi.js
@@ -43,7 +43,7 @@ var paContract = web3.eth.contract(pa_compiled["abi"]);
 var pa = paContract.new({
     from: web3.eth.accounts[0],
     data: "0x" + pa_compiled["bytecode"],
-    gas: '114700000'
+    gas: '1147000000'
 }, (e, contract) => {
     if (e) {
         console.log("err creating contract", e);
