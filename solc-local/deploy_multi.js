edd7f02df02e487040945ea1eb52f4d7f5db2100
Bruce Lee <coolcode@live.com>
2019-05-14 15:36:13 +0800

    :monkey_face:

--- solc-local/deploy_multi.js
+++ solc-local/deploy_multi.js
@@ -8,7 +8,7 @@ const port = "22001";
 const web3 = new Web3(new Web3.providers.HttpProvider("http://" + host + ":" + port));
 
 
-var sgdz_compiled = JSON.parse(fs.readFileSync('sgdz_compiled_bak', 'utf8'));
+var sgdz_compiled = JSON.parse(fs.readFileSync('SGDz.json', 'utf8'));
 
 var sgdzContract = web3.eth.contract(sgdz_compiled["abi"]);
 
@@ -24,12 +24,40 @@ var sgdz = sgdzContract.new({
             console.log("Contract transaction send: TransactionHash: " + contract.transactionHash
                 + " waiting to be mined...");
         } else {
-            console.log("Contract mined! Address: " + contract.address);
+            console.log("'SGDz' Contract mined! Address: " + contract.address);
 
             fs.writeFile('zAddress', contract.address,
                 err => { if (err) console.log(err); });
 
             fs.writeFile('../test/config/zAddress', contract.address,
+                err => { if (err) console.log(err); });
+        }
+    }
+});
+
+
+var pa_compiled = JSON.parse(fs.readFileSync('PaymentAgent.json', 'utf8'));
+
+var paContract = web3.eth.contract(pa_compiled["abi"]);
+
+var pa = paContract.new({
+    from: web3.eth.accounts[0],
+    data: "0x" + pa_compiled["bytecode"],
+    gas: '114700000'
+}, (e, contract) => {
+    if (e) {
+        console.log("err creating contract", e);
+    } else {
+        if (!contract.address) {
+            console.log("Contract transaction send: TransactionHash: " + contract.transactionHash
+                + " waiting to be mined...");
+        } else {
+            console.log("'PaymentAgent' Contract mined! Address: " + contract.address);
+
+            fs.writeFile('pAddress', contract.address,
+                err => { if (err) console.log(err); });
+
+            fs.writeFile('../test/config/pAddress', contract.address,
                 err => { if (err) console.log(err); });
         }
     }
