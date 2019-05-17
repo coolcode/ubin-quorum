33aa528bfe3ffd85032ed62e13eba791ec217cfa
Bruce Lee <coolcode@live.com>
2019-05-17 09:33:34 +0800

    :monkey_face: nodejs test contracts

--- test/BlockService.js
+++ test/BlockService.js
@@ -1,5 +1,5 @@
 import Web3 from "web3";
-import BN from "bn.js";
+import BN from "../node_modules/bn.js/lib/bn";
 import fs from 'fs';
 
 import config from "./config/config.json";
@@ -33,17 +33,17 @@ export default class BlockService {
 
     async deployContracts() {
         console.log("names:", this.names);
-        await this.deployContract(this.names[2]);
         for (let i = 0; i < this.names.length; i++) {
             await this.deployContract(this.names[i]);
         }
 
-        await this.deployContract(this.names[2]);
-        await this.Bank.setExternalContracts(this.StashFactory.address, this.PledgeAgent.address, this.RedeemAgent.address).send({from: this.owner, gas: this.defaultGas});
+        const r = await this.Bank.setExternalContracts(this.StashFactory.address, this.PledgeAgent.address, this.RedeemAgent.address).send({from: this.owner, gas: this.defaultGas});
+        console.log(`set bank's external contracts. tx hash: `, r.transactionHash);
     }
 
 
     async deployContract(name) {
+        console.log(`deploying "${name}"...`);
         const sgdz_compiled = JSON.parse(fs.readFileSync(`../build/contracts/${name}.json`, 'utf8'));
         //const web3Contract = new this.web3.eth.Contract(sgdz_compiled["abi"]);
         /*, null, {
@@ -53,69 +53,47 @@ export default class BlockService {
         }*/
         //const deployAgent = new MethodProxy(this, web3Contract.deploy, );
         //const self = this;
-        console.log(`deploying "${name}"...`);
 
-        const transactionHash = await this.deployAgent({
+        const receipt = await this.deployAgent({
             abi: sgdz_compiled["abi"],
             data: sgdz_compiled["bytecode"],
             from: this.owner,
             gas: this.defaultGas
         });
 
-        const receipt = await this.getReceipt(transactionHash);
+        //const receipt = await this.getReceipt(transactionHash);
         const contractAddress = receipt.contractAddress;
         console.log(`addr:`, contractAddress);
         const contract = this.loadContract(name, sgdz_compiled["abi"], contractAddress);
         console.log(`"${name}": "${contract.address}",`);
-        fs.writeFile('config/' + name + '_Address', contract.address,
-            err => {
-                if (err) console.log(err);
-            });
+        // fs.writeFile('config/' + name + '_Address', contract.address,
+        //     err => {
+        //         if (err) console.log(err);
+        //     });
 
-        return contract;
+        //return contract;
+        return true;
     };
 
-    async deployAgent(opt) {
+    deployAgent(opt) {
         const {abi, data, from, gas} = opt;
-        const web3Contract = new this.web3.eth.Contract(abi);
         const self = this;
         return new Promise(function (resolve, reject) {
-            web3Contract.deploy({
-                data: data
-            }).send({
-                from: from,
-                gas: gas
-            }, async (error, transactionHash) => {
+            const web3Contract = new self.web3.eth.Contract(abi);
+            web3Contract.deploy({data: data}).send({from: from, gas: gas}, async (error, hash) => {
                 if (error) {
                     console.error(error);
                     reject(error);
                     return;
                 }
-                await self.confirmTx(transactionHash);
-                resolve(transactionHash);
+                console.log("tx hash: ", hash);
+                await self.confirmTx(hash);
+                //resolve(hash);
+                console.log("confirmed: ", hash);
+                const receipt = await self.getReceipt(hash);
+                //console.log("receipt: ", receipt);
+                resolve(receipt);
             });
-            /*.on('error', (error) => {
-            console.error(error);
-            reject(error);
-        })
-        .on('transactionHash', (transactionHash) => {
-            console.log("transactionHash:", transactionHash)
-        })
-        .on('receipt', (receipt) => {
-            console.log("contractAddress:", receipt.contractAddress)
-        })
-        .on('confirmation', (confirmationNumber, receipt) => {
-            console.log("confirmationNumber:", confirmationNumber, "contractAddress:", receipt.contractAddress);
-            resolve(receipt);
-        });*/
-            //*, async (error, transactionHash) => {
-            //                 if (error) {
-            //                     console.error(error);
-            //                     reject(error);
-            //                     return;
-            //                 }
-            //                 //resolve(transactionHash);
-            //             }*/
         });
     }
 
