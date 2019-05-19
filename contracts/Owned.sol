f3290a1bca35c8082a8f658a67c0fc8114659730
Bruce Lee <coolcode@live.com>
2019-05-19 19:00:22 +0800

    :monkey_face: upgrade solidity to 0.4.24

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -1,22 +1,41 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.24;
 
-contract Owned{
-  address owner;
+contract Owned {
+    address owner;
 
-  function Owned() {
-    owner = msg.sender;
-  }
+    function Owned() {
+        owner = msg.sender;
+    }
 
-  modifier onlyOwner() {
-    _;
-    //if(msg.sender!=owner) throw; _;
-  }
+    modifier onlyOwner() {
+        _;
+        //if(msg.sender!=owner) throw; _;
+    }
 
-  function getOwner() view returns (address) {
-    return owner;
-  }
+    function getOwner() view returns (address) {
+        return owner;
+    }
 
-  function changeOwner(address _newOwner) onlyOwner {
-    owner = _newOwner;
-  }
+    function changeOwner(address _newOwner) onlyOwner {
+        owner = _newOwner;
+    }
+
+    function stringToBytes32(string memory source) returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+
+    function bytes32ToString(bytes32 source) returns (string){
+        bytes memory bytesArray = new bytes(32);
+        for (uint256 i = 0; i < 32; i++) {
+            bytesArray[i] = source[i];
+        }
+        return string(bytesArray);
+    }
 }
--- contracts/ZSLPrecompile.sol
+++ contracts/ZSLPrecompile.sol
@@ -1,4 +1,4 @@
-/* pragma solidity ^0.4.11; */
+/* pragma solidity ^0.4.24; */
 
 // Copyright 2017 Zerocoin Electric Coin Company LLC
 //
